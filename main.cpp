e12d9f42c29c9ade99608bc99ebeeab3c1c6ddd9
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2023-12-05 10:09:01 +0900

    1-4 完了

--- main.cpp
+++ main.cpp
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <time.h>
+#include <functional>
 
-typedef void (*PFunc)(int*);
+//typedef void (*PFunc)(int*);
+//
+//void DispResult(int* s) {
+//	
+//	
+//}
 
-void DispResult(int* s) {
-	
-	printf("%d秒待って実行されたよ\n", *s);
-}
+void setTimeout(std::function<void(int*)>p, int &second) {
 
-void setTimeout(PFunc p, int second) {
-
-	Sleep(second * 1000);
 	p(&second);
+	Sleep(second * 1000);
 }
 
 int main() {
 
+	std::function<void(int*)> fx = [](int* s) { printf("%d秒待って実行されたよ\n", *s); };
 
 	srand((unsigned int)time(nullptr));
 
 	int dice = rand() % 6 + 1;
 
 	int userAnswer = 0;
+	int time = 3;
+
 	printf("奇数か偶数化を予想する、偶数なら　0, 奇数なら1を入力してください");
 	scanf_s("%d", &userAnswer);
 
-	PFunc p;
-	p = DispResult;
-	setTimeout(p, 3);
+	//PFunc p;
+	//p = DispResult;
+	setTimeout(fx, time);
 
 	if (dice % 2 == userAnswer) 
 	{
