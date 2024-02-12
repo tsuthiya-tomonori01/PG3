ba0a2d587b0a0a74c72a4d6fce4a59615d68b7c2
tsuthiya-tomonori01 <k022g0017@g.neec.ac.jp>
2024-02-13 02:05:43 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -1,32 +1,44 @@
 ﻿#include <stdio.h>
-#include "Template.h"
 
-template<typename Type>
+template<typename Type1, typename Type2>
 
 class Template {
 
 public:
-	Type a;
-	Type b;
+	Type1 a;
+	Type2 b;
 
-	Type Template(Type a, Type b) : a(a), b(b){};
+	Template(Type1 a, Type2 b) : a(a), b(b) {}
 
-	Type Min(Type a, Type b) {
+	Template Min(Type1 a, Type2 b) {
 
 		if (a < b)
 		{
-			return static_cast<Type>(a);
+			return static_cast<Type1>(a);
 		} 
 		else 
 		{
-			return static_cast<Type>(b);
+			return static_cast<Type2>(b);
 		}
 	}
 };
+
 int main()
 {
-	Template<int> a();
-	Template<float> b();
+	Template<int, int> a(10,20);
+	Template<int,float> b(10,20);
+	Template<int, double> c(10,20);
+	Template<float,double> d(10,20);
+	Template<float, float> e(10,20);
+	Template<double, double> f(10,20);
+
+
+	printf("int int %d\n", a.Min(1, 2));
+	printf("int float %d\n", b.Min(1, 2.0));
+	printf("int double %lf\n", c.Min(1, 2.0));
+	printf("double float %lf\n", d.Min(1, 2.0));
+	printf("float float %f\n", e.Min(1.0, 2.0));
+	printf("double double %lf\n", f.Min(1.0, 2.0));
 
 	return 0;
 }
